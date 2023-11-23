b6dbe146403cb12e43af1ceecc68763c96dee4fd
NahuelLu <nahuellucana1999@gmail.com>
2023-11-23 16:52:48 -0300

    Add some category controllers with implementation and other without implementation, and some very poor views related with that controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,10 +1,63 @@
 const CategoryModel = require("../models/category")
 const asyncHandler = require("express-async-handler");
-
-exports.get_all_categories = asyncHandler(async (req, res, next) => {
+const get_all_categories = asyncHandler(async (req, res, next) => {
     const categories = await CategoryModel.find().exec()
     res.render("category", {
         title: "Categories",
         categories: categories
     })
 });
+const category_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT implemented to create a category GET")
+})
+const category_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT implemented to create a category POST")
+})
+const category_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT implemented to update this category with ID:"+req.params.id+",GET")
+})
+const category_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT implemented to update this category with ID:"+req.params.id+", POST")
+}) 
+const category_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT implemented to delete a category GET")
+})
+const category_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT implemented to delete a category POST")
+}) 
+const category_detail_get = asyncHandler(async (req, res, next) => {
+    const category = await CategoryModel.findById(req.params.id).exec()
+    if(!category){
+        res.render("category_error",{
+            title: "This category doesn't exists",
+        })
+    }else{
+        res.render("category_detail",{
+            title: category.name + "detail",
+            category
+        })
+    }
+}) 
+
+
+
+
+
+
+
+
+//Category' Controllers
+const CategoryController = {
+    get_all_categories,
+    category_create_get,
+    category_create_post,
+    category_update_get,
+    category_update_post,
+    category_delete_get,
+    category_delete_post,
+    category_detail_get
+}
+
+module.exports = CategoryController
+
+
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -4,5 +4,12 @@ const categoryController = require("../controllers/categoryController")
 
 
 router.get("/categories",categoryController.get_all_categories)
+router.get("/create",categoryController.category_create_get)
+router.post("/create",categoryController.category_create_post)
+router.get("/:id/update",categoryController.category_update_get)
+router.post("/:id/update",categoryController.category_update_post)
+router.get("/:id/delete",categoryController.category_delete_get)
+router.post("/:id/delete",categoryController.category_delete_post)
+router.get("/:id",categoryController.category_detail_get)
 
 module.exports = router;
