b305d7ab8c0ce68065d50c184e204982a6de513e
NahuelLu <nahuellucana1999@gmail.com>
2023-11-29 09:06:01 -0300

    Added controllers to update an item and its related views

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -43,11 +43,45 @@ const item_create_post = [
         }
     })
 ]
-const item_update_get = asyncHandler(async (req, res, next) => {
+const item_update_get = asyncHandler(async (req, res) => {
+    const item = await ItemModel.findById(req.params.id).exec()
+    const categories = await CategoryModel.find().exec()
+    res.render("item_form",{
+        title: "Update Item",
+        categories,
+        item
+    })
 });
 const item_update_post = [
+    body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("description", "Description must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("category", "Category must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("price", "Price must not be empty").trim().isLength({ min: 1 }).escape(),
+    body("stock", "Stock must not be empty").trim().isLength({ min: 1 }).escape(),
     asyncHandler(async (req, res, next) => {
-})
+        const errors = validationResult(req)
+        const item = new ItemModel({ 
+            name: req.body.name , 
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price, 
+            number_in_stock: req.body.stock,
+            _id: req.params.id
+        })
+        if(!errors.isEmpty()){
+            const categories = await CategoryModel.find().exec()
+            res.render("item_form",{
+                title: "Update Item",
+                categories,
+                item,
+                errors
+            })
+        }else{
+            const itemUpdated = await ItemModel.findByIdAndUpdate(req.params.id,item).exec()
+            res.redirect(itemUpdated.url)
+        }
+
+    })
 ]
 const item_delete_get = asyncHandler(async (req, res, next) => {
 });
